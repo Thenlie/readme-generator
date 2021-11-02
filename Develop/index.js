efb7c4681d066cb259e29dfde8aa5b4d6ab619ae
Leithen <leithen113@gmail.com>
2021-11-02 15:18:59 -0600

    Added logic to create license badge

--- Develop/index.js
+++ Develop/index.js
@@ -1,6 +1,7 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+const { generateMarkdown } = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
 const questions = () => {
@@ -55,7 +56,10 @@ function writeToFile(fileName, data) {}
 function init() {
     questions() // Prompt user to get input data
         .then(input => {
-            console.log(input);
+            return generateMarkdown(input);
+        })
+        .then(markdown => {
+            console.log(markdown)
         })
 }
 
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,6 +1,13 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+    if (license) {
+      return `https://img.shields.io/badge/license-${license}-brightgreen`
+    } else {
+      return '';
+    }
+};
+
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -8,13 +15,22 @@ function renderLicenseLink(license) {}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  return `
+    ## License
+
+    ${renderLicenseBadge(license)}
+  `
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-
-`;
+  const { license } = data;
+  return `
+  # ${data.title}
+  ## License
+  ${renderLicenseBadge(license)}
+  `;
 }
 
-module.exports = generateMarkdown;
+module.exports = { generateMarkdown };
