b27ce7094496fd23d3022b690eab380c492572a0
Leithen <leithen113@gmail.com>
2021-11-02 14:04:32 -0600

    Created list of questions to ask user

--- Develop/index.js
+++ Develop/index.js
@@ -1,14 +1,64 @@
 // TODO: Include packages needed for this application
-inquirer = require('inquirer');
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = () => {
+    return inquirer.prompt([{
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?'
+    }, {
+        type: 'input',
+        name: 'user',
+        message: 'What is your GitHub username?'
+    }, {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?'
+    }, {
+        type: 'input',
+        name: 'description',
+        message: 'What is the descritption of your project?'
+    }, {
+        type: 'input',
+        name: 'install',
+        message: 'What are the installation instructions for you project?'
+    }, {
+        type: 'input',
+        name: 'usage',
+        message: 'What is the usage information for your project?'
+    }, {
+        type: 'input',
+        name: 'contribution',
+        message: 'What are the contribution guidelines for your project?'
+    }, {
+        type: 'input',
+        name: 'test',
+        message: 'What are the test instruction for your project?'
+    }, {
+        type: 'list',
+        name: 'license',
+        message: 'What is the license of your project?',
+        choices: ['MIT', 'ISC', 'Apache', 'GPL'],
+        default: 'MIT'
+    }])
+    .then(input => {
+        return input;
+    })
+};
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    questions() // Prompt user to get input data
+        .then(input => {
+            console.log(input);
+        })
+}
 
 // Function call to initialize app
-init();
+init()
+
